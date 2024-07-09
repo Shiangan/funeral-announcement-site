32462c1bc21ae37c91b92b382d54c78e07dc625c
Shiangan <avarubby85110@gmail.com>
2024-07-09 22:05:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,20 +24,44 @@ function generateObituaryAndRedirect() {
     var funeralLocation = document.getElementById('funeral-location').value;
     var textStyle = document.getElementById('text-style').value;
     var sendFlowerBasket = document.getElementById('send-flower-basket').value;
+    var musicSelection = document.getElementById('music').value;
 
-    // 組合訃聞內容
-    var obituaryTitle = '訃聞標題';
-    var obituaryText = '往生者姓名: ' + deceasedName + '<br>' +
-                       '逝世日期: ' + deathDate + '<br>' +
-                       '出殯日期: ' + funeralDate + '<br>' +
-                       '出殯地點: ' + funeralLocation + '<br>' +
-                       '內文風格: ' + textStyle + '<br>' +
-                       '是否贈送花籃: ' + sendFlowerBasket;
+   // 計算年齡
+    var deathYear = new Date(deathDate).getFullYear();
+    var birthYear = deathYear - 80; // 假設80歲
+    var age = deathYear - birthYear;
+
+    // 根據性別和年齡條件選擇訃聞內容
+    var obituaryContent;
+    if (gender === 'male' && age <= 30) {
+         obituaryContent = `我們摯愛的${deceasedName}先生，已於國曆${deathDate}辭世往生，得年${age}歲。<br>謹擇於國曆${funeralDate}於${funeralLocation}舉行追思告別儀式，敬邀親朋好友蒞臨告別致意。`;
+    } else if (gender === 'female' && age <= 30){
+    if (gender === 'male' && age <= 60) {
+        obituaryContent = `我們摯愛的${deceasedName}先生，已於國曆${deathDate}辭世往生，享年${age}歲。<br>謹擇於國曆${funeralDate}於${funeralLocation}舉行追思告別儀式，敬邀親朋好友蒞臨告別致意。`;
+    } else if (gender === 'female' && age <= 60) {
+        obituaryContent = `我們摯愛的${deceasedName}先生，已於國曆${deathDate}辭世往生，享年${age}歲。<br>謹擇於國曆${funeralDate}於${funeralLocation}舉行追思告別儀式，敬邀親朋好友蒞臨告別致意。`;
+    } else if (gender === 'female' && age <= 90) {
+        obituaryContent = `我們摯愛的${deceasedName}女士，已於國曆${deathDate}辭世往生，享嵩壽${age}歲。<br>謹擇於${funeralDate}於${funeralLocation}舉行追思告別儀式，敬邀親朋好友蒞臨告別致意。`;
+    } else if (gender === 'female' && age <= 100) {
+        obituaryContent = `我們摯愛的${deceasedName}，已於國曆${deathDate}辭世往生，享耆壽${age}歲。<br>謹擇於${funeralDate}於${funeralLocation}舉行追思告別儀式，敬邀親朋好友蒞臨告別致意。`;
+    }
 
     // 將訃聞內容存儲到 sessionStorage 中，以便在下一頁訪問
-    sessionStorage.setItem('obituaryTitle', obituaryTitle);
-    sessionStorage.setItem('obituaryText', obituaryText);
+    sessionStorage.setItem('obituaryContent', obituaryContent);
+
+    // 根據音樂選擇播放不同音樂
+    playMusic(musicSelection);
 
     // 跳轉到 page1.html 頁面
     window.location.href = 'page1.html';
+}
+
+function playMusic(selection) {
+    var audio = new Audio();
+    if (selection === 'music1') {
+        audio.src = 'path/to/music1.mp3'; // 更換為音樂1的路徑
+    } else if (selection === 'music2') {
+        audio.src = 'path/to/music2.mp3'; // 更換為音樂2的路徑
+    }
+    audio.play();
 }
